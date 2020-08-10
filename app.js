2bd5656ee5f1aebfc9e811779961a65c98f7b168
alexsiby <69440103+alexsiby@users.noreply.github.com>
2020-08-10 07:27:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBz_JwhJMA4KoHpvnupqIlwGQs6JlNL8JY",
+  authDomain: "free-dimond-10c7b.firebaseapp.com",
+  databaseURL: "https://free-dimond-10c7b.firebaseio.com",
+  projectId: "free-dimond-10c7b",
+  storageBucket: "free-dimond-10c7b.appspot.com",
+  messagingSenderId: "145858380032",
+  appId: "1:145858380032:web:64c7ab3726a963b32333e5",
+  measurementId: "G-DZHFG1DS0K"
 };
 
      
